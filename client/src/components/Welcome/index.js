b088b9e1a597450848e384ec657de3506c6fabd9
JDLong <jdlong1980@gmail.com>
2019-05-10 11:44:30 -0400

    completed welcome page

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -30,64 +30,70 @@ function Welcome({ children }) {
         </p>
         <br />
 
-        <div className="columns">
-          <div className="column">
+        <div className="column">
             <div class="card welcome-card">
               <header class="card-header">
                 <p class="card-header-title">Getting Started</p>
               </header>
-              <div class="card-content">
-                The menu on the left lists web development categories. Each category you click on will display resources for you to explore. If you'd like to keep track of what resources you've seen, save your favorites and share likes, please login/register. Logging in will also allow you to contact the administrator to report bugs, suggest a site to add, or just say hello!
+            <div class="card-content">
+              <p>
+                The menu on the left lists web development categories. Each
+                category you click on will display resources for you to explore.</p><br/><p>
+                If you'd like to keep track of what resources you've seen, save
+                your favorites and share likes, please login/register. Logging
+                in will also allow you to contact the administrator to report
+                bugs, suggest a site to add, or just say hello!</p>
               </div>
             </div>
           </div>
           <div className="column">
-            <div class="card welcome-card">
+            <div class="card welcome-card" id="about">
               <header class="card-header">
                 <p class="card-header-title">About the Author</p>
               </header>
               <div class="card-content">
-                <p>This web app was created by Jamie DeLong using the MERN stack:</p><br/>
                 <p>
-                <span>
-                <i class="icon devicon-mongodb-plain-wordmark colored"></i>
+                  This web app was created by Jamie DeLong using the MERN stack:
+                </p>
+                <br />
+                <span className="icon">
+                  <i className="devicon-mongodb-plain-wordmark colored" />
+                </span>
+                <span className="icon">
+                  <i className="devicon-express-original-wordmark" />
                   </span>
-                </p>
-                <p>
-
-                  <i class="icon devicon-express-original-wordmark"></i>
-                </p>
-<p>
-                  <span><i class="icon devicon-react-original"></i></span>
-                </p>
-                <p>
-                  <span><i class="icon devicon-nodejs-plain-wordmark"></i></span>
-                  </p>
+                  <span className="icon">
+                  <i className="devicon-react-original-wordmark" />
+                  </span>
+                  <span className="icon">
+                  <i className="devicon-nodejs-plain-wordmark" />
+                </span>
+                
               </div>
               <footer class="card-footer">
                 <p class="card-footer-item">
                   <span>
-                    Website <br/>
-                    <a href="https://milleniyum.github.io/Portfolio/">
+                    Website <br />
+                    <a href="https://milleniyum.github.io/Portfolio/" rel="noopener noreferrer" target="_blank">
                       Jamie DeLong
                     </a>
                   </span>
                 </p>
                 <p class="card-footer-item">
                   <span>
-                    Projects <a href="https://github.com/Milleniyum">Github</a>
+                    View this Project on <br/> <a href="https://github.com/Milleniyum/WebDevDepot" rel="noopener noreferrer" target="_blank">Github</a>
                   </span>
                 </p>
                 <p class="card-footer-item">
                   <span>
-                    Networking <a href="www.linkedin.com/in/jamieldelong">LinkedIn</a>
+                    Network with Him on <br/>
+                    <a href="https://www.linkedin.com/in/jamieldelong" rel="noopener noreferrer" target="_blank">LinkedIn</a>
                   </span>
                 </p>
               </footer>
             </div>
           </div>
         </div>
-      </div>
     </React.Fragment>
   );
 }
